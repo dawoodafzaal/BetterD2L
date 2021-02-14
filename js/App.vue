9ff627dadf9a0650d3824495c5999290537ed4bf
Rupert Amodia <rupertraphael@live.com>
2021-02-13 20:41:53 -0700

    wip: frontend

---
+++ js/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<nav class="fixed h-screen w-1/4 bg-blue-600">
+			<section class="px-8 py-4">
+				<div class="grid grid-cols-2 gap-4">
+					<div class="py-16 bg-blue-500 rounded-lg text-white text-center">
+						<router-link to="/calendar" class="text-lg font-bold text-white">Calendar</router-link>
+					</div>
+					<div class="py-16 bg-blue-500 rounded-lg text-white text-center">
+						<router-link to="/checklist" class="text-lg font-bold text-white">Checklist</router-link>
+					</div>
+					<div class="py-16 bg-blue-500 rounded-lg text-white text-center">
+						<router-link to="/communication" class="text-lg font-bold text-white">Communication</router-link>
+					</div>
+					<div class="py-16 bg-blue-500 rounded-lg text-white text-center">
+						<router-link to="/dropbox" class="text-lg font-bold text-white">Dropbox</router-link>
+					</div>
+				</div>
+			</section>
+		</nav>
+		<section class="w-3/4 min-h-screen" style="margin-left: 25%;">
+			<router-view></router-view>
+		</section>
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => {
+		return {
+
+		};
+	}
+}
+</script>
---
+++ js/checklist/index.vue
@@ -0,0 +1,56 @@
+<template>
+	<div class="py-8 px-8">
+		<header class="text-center">
+			<h1 class="text-5xl border-b-2 border-blue-600 font-bold">Checklist</h1>
+		</header>
+		<table class="w-full">
+			<tr v-for="item in items">
+				<td><md-checkbox v-model="item.checked_at"></md-checkbox></td>
+				<td class="text-xl">
+					<router-link v-if="item.related" :to="`${item.related.type}/${item.related.id}`">{{ item.title }}</router-link>
+					<span v-else>{{ item.title }}</span>
+				</td>
+				<td class="uppercase text-right">{{ item.due_on }}</td>
+			</tr>
+		</table>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			items: [
+				{
+					id: 1,
+					title: 'Submit Worksheet 5 (CPSC 329 Dropbox)',
+					related: {
+						type:'dropbox',
+						id: 1
+					},
+					checked_at: false,
+					due_on: 'Tomorrow',
+				},
+				{
+					id: 2,
+					title: 'Start Computing Machinery Project',
+					checked_at: true,
+					due_on: 'Yesterday',
+				},
+				{
+					id: 3,
+					title: 'Study for STAT 213',
+					checked_at: true,
+					due_on: 'Tomorrow',
+				},
+				{
+					id: 4,
+					title: 'Clean desk',
+					checked_at: false,
+					due_on: 'Last Week',
+				},
+			]
+		};
+	},
+}
+</script>
