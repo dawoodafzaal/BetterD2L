d6339c47abe93ca00c4b2523baf78272efe25e5e
Rupert Amodia <rupertraphael@live.com>
2021-02-14 06:21:48 -0700

    Add Checklist request.

---
+++ js/api/ChecklistRequest.js
@@ -0,0 +1,21 @@
+import Request from './Request';
+
+class ChecklistRequest extends Request{
+    retrieve() {
+        return this.get('http://127.0.0.1:5000/checklist');
+    }
+
+    store() {
+    	return this.post('http://127.0.0.1:5000/checklist');
+    }
+
+    update(id) {
+    	return this.patch('http://127.0.0.1:5000/checklist/' + id);
+    }
+
+    destroy(id) {
+        return this.delete('http://127.0.0.1:5000/checklist/' + id);
+    }
+}
+
+export default DropboxRequest
