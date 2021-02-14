d3752a37d80601029f1b93fc08aab18b7a839e3e
Razeen Adil <razeen.adil16@gmail.com>
2021-02-14 05:38:51 -0700

    hi

---
+++ Mental Health/mentalBackend.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask import jsonify
+from flask_cors import CORS, cross_origin
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import requests
+
+app = Flask(__name__)
+STUDENT_ID = 195
+headers = {
+  'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjYzYzc5ZjIzLWZiMTAtNGI4YS1hMjdlLWI4YTc4NGJmNWI5MCJ9.eyJpc3MiOiJodHRwczovL2FwaS5icmlnaHRzcGFjZS5jb20vYXV0aCIsImF1ZCI6Imh0dHBzOi8vYXBpLmJyaWdodHNwYWNlLmNvbS9hdXRoL3Rva2VuIiwiZXhwIjoxNjEzMjU0MjM1LCJuYmYiOjE2MTMyNTA2MzUsInN1YiI6IjEyNTUiLCJ0ZW5hbnRpZCI6IjUyNzMxMzE3LTM3MmEtNGRiNC1iNDlkLTQ5NTAyY2U0M2YyYiIsImF6cCI6IjVkMjc0NTBmLTJjZTktNGRjZS05NTcwLTBiMjM0NTgwZGZkZCIsInNjb3BlIjoiY29udGVudDoqOiogY29yZToqOiogZGF0YWh1YjoqOiogZGlzY3Vzc2lvbnM6KjoqIGVucm9sbG1lbnQ6KjoqIGdyYWRlczoqOiogcXVpenppbmc6KjoqIHJlcG9ydGluZzoqOiogcm9sZToqOiogdXNlcnM6KjoqIiwianRpIjoiNGUyOTdkYTEtZDg4Ni00ODI3LTgyN2ItZmE4OGM0MWM5ZjExIn0.k4ndXz5MuMjwu20AXhb6kpOwfVy-iClslOsm7XglIrzO1TKBy1key-iO0MDOhNrAC5A4CpZyWUk3VtOLL8DAM-RGRSXYxEBXpJ8Hnbb2kgUVtBU9ZB7kagfulIRihH5lgEIX08smsokviWowZVT2pUNWxBFpIoJjCdIoXTJ5NAis0pVTPa0EzjELnNFuSuGk91X8Jpv5Gh7sFzDerm9rgjHCzi6it3hW--7Mxh4Rpge8dbEinbC3ugbj4YeCorkScTH65prXGyzbovX1cuNAqTyst0sRxNC7PG0F__d-OW7Q--SYckrYSdMkkg8Q19g2UpQI9pbzk23tGifeFGlHlQ',
+  'Cookie': 'ADRUM=s=1613250634620&r=https%3A%2F%2Fdevcop.brightspace.com%2Fd2l%2FsystemCheck%2Fwidget%3F0; d2lSessionVal=TKH40vprvvlOxHs5ZGSliFnXb; d2lSecureSessionVal=Y2iwueXFvbe2Ygxmc183PA5zw'
+}
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+#initalize bot
+health_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStroageAdapter")
+
+#
+trainer = ChatterBotCorpusTrainer(health_bot)
+
+#
+trainer.train("chatterbot.corpus.english")
+
+
+@app.route('/mentalHealth')
+@cross_origin()
+def mentalHealth():
+    userText = request.args.get('msg')
+    return str(health_bot.get_response(userText))
+
+
+if __name__ == '__main__':
+    app.run()
+        
+
+    
+
+
